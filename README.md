cef5345af7e1a1dc3c1a8c209bde30401ddd378c
andy <andy@macbookpro>
2024-11-19 22:03:06 +0800

    完成

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # cctv_weather_forecasting_m3u
 cctv 中央电视台 新闻联播 天气预报 m3u生成
+
+https://zh.wikipedia.org/zh-tw/%E6%96%B0%E9%97%BB%E8%81%94%E6%92%AD%E5%A4%A9%E6%B0%94%E9%A2%84%E6%8A%A5
+
+https://www.weathertv.cn/cms/yb_video.shtml
+
+`node app.js`
---
+++ app.js
@@ -0,0 +1,38 @@
+// https://www.weathertv.cn/cms/yb_video.shtml
+const puppeteer = require('puppeteer');
+
+(async () => {
+
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  const baseUrl='https://www.weathertv.cn';
+  await page.goto(`${baseUrl}/cms/yb_video.shtml`, { waitUntil: 'networkidle2' });
+//   const links = await page.$$('#ylist > li > div > div.img-box > a');
+  const links = await page.$$('#ylist > li > div > p > a');
+//   console.log(links);
+  for (let i = 0; i < links.length; i++) {
+    const link = links[i];
+    const href = await page.evaluate(el => el.getAttribute('href'), link);
+    const text = await page.evaluate(el => el.innerText, link);
+    let urlOf1Date =`${baseUrl}${href}`; 
+    
+    const page1 = await browser.newPage();
+    await page1.goto(urlOf1Date, { waitUntil: 'networkidle2' });
+    const videoElement = await page1.$('video source');
+    const videoUrl = await page1.evaluate(el => el.getAttribute('src'), videoElement);
+    await page1.close();
+
+    // console.log({ href, videoUrl });
+    const date = text.replace('新闻联播天气预报', '').replace(/(\d{4})(\d{2})(\d{2})/, '$1-$2-$3');
+    // // console.log({date, videoUrl});
+
+    // console.log({date, urlOf1Date,videoUrl});
+
+
+    console.log(`#EXTINF:-1,${date}\n${videoUrl}\n`);
+    // page.goBack();
+  }
+  await browser.close();
+})();
+
+
