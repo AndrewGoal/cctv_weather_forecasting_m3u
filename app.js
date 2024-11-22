e85a86f8268a7e8996c16d7721fe792f8b9a3f03
andy <andy@macbookpro>
2024-11-22 14:07:33 +0800

    农业气象

--- app.js
+++ app.js
@@ -8,18 +8,20 @@
         'https://www.weather.com.cn/pubm/cctv4.htm',
         'https://www.weather.com.cn/pubm/tiyu.htm',
         'https://www.weather.com.cn/pubm/cctv7.htm',
-        'https://www.weather.com.cn/pubm/cgtn.htm'
+        // 'https://www.weather.com.cn/pubm/cgtn.htm',
+        // 'https://www.weather.com.cn/pubm/nongye.htm'
     ];
     
     let group_names = [
         '中央一晚',
-        '中央一早',
+        '中央13早',
         '中央一午',
         '中央二早',
         '中央四',
-        '中央五',
+        '体育天气',
         '中央七',
-        '英文台'
+        // '英语台',
+        // '农业气象'
     ];
     
     console.log(`#EXTM3U`);
@@ -27,27 +29,21 @@
     for (let i = 0; i < urls.length; i++) {
         const url = urls[i];
         const group_name = group_names[i];
-    
         let res = await fetch(`${url}?callback=getLbDatas&_=${Date.now()}`, {
+            "cache": "default",
+            "credentials": "include",
             "headers": {
-              "accept": "text/javascript, application/javascript, application/ecmascript, application/x-ecmascript, */*; q=0.01",
-              "accept-language": "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7",
-              "cache-control": "no-cache",
-              "pragma": "no-cache",
-              "sec-ch-ua": "\"Google Chrome\";v=\"131\", \"Chromium\";v=\"131\", \"Not_A Brand\";v=\"24\"",
-              "sec-ch-ua-mobile": "?0",
-              "sec-ch-ua-platform": "\"macOS\"",
-              "sec-fetch-dest": "empty",
-              "sec-fetch-mode": "cors",
-              "sec-fetch-site": "same-origin",
-              "x-requested-with": "XMLHttpRequest",
-              "cookie": "HttpOnly; userNewsPort0=1; defaultCty=101120201; defaultCtyName=%u9752%u5C9B; f_city=%E9%9D%92%E5%B2%9B%7C101120201%7C",
-              "Referer": "https://www.weather.com.cn/video/ylist.shtml",
-              "Referrer-Policy": "strict-origin-when-cross-origin"
+                "Accept": "*/*",
+                "Accept-Language": "zh-CN,zh-Hans;q=0.9",
+                "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 16_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.4 Mobile/15E148 Safari/604.1"
             },
-            "body": null,
-            "method": "GET"
+            "method": "GET",
+            "mode": "cors",
+            "redirect": "follow",
+            "referrer": "http://www.weather.com.cn/",
+            "referrerPolicy": "strict-origin-when-cross-origin"
         });
+
         let text = await res.text();
         let data = JSON.parse(text.slice(text.indexOf('(') + 1, text.lastIndexOf(')')));
         // console.log(data);
