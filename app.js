0bafd8bd4ff46ae5b3fac36ed5fcbe36bd3a0e94
andy <andy@macbookpro>
2024-11-24 16:05:20 +0800

    开关 + 格式化

--- app.js
+++ app.js
@@ -3,180 +3,194 @@
     let latests = [];
     let m3utext = '';
 
-    let cateids = [101559,101529];
-    let group_sd_names = ["山东新闻晚","山东卫视早"];
-    let group_sd_times = ["19:31","06:56"];
+    if (1) {
+        // 山东气象，数据源3，4
+
+        let cateids = [101559, 101529];
+        let group_sd_names = ["山东新闻晚", "山东卫视早"];
+        let group_sd_times = ["19:31", "06:56"];
 
 
-    for (let index = 0; index < cateids.length; index++) {
-        let cateid = cateids[index];
-        let now = new Date();
-        let latestIndex = 0;
-        for (let i = 0; i < 5; i++) {
-            let date = new Date(now.getTime() - i * 24 * 60 * 60 * 1000);
-            let dateISOStringSD = toISODateFMTbyTimezoneOffset(date, -480).slice(0, 10);
-            // console.log(dateISOStringSD);
-            let res_sd = await fetch(`https://sdxw.iqilu.com/v2/app/media/program/video/lists?cateid=${cateid}&plat=cq&date=${dateISOStringSD}&page=1`, {
+        for (let index = 0; index < cateids.length; index++) {
+            let cateid = cateids[index];
+            let now = new Date();
+            let latestIndex = 0;
+            for (let i = 0; i < 5; i++) {
+                let date = new Date(now.getTime() - i * 24 * 60 * 60 * 1000);
+                let dateISOStringSD = toISODateFMTbyTimezoneOffset(date, -480).slice(0, 10);
+                // console.log(dateISOStringSD);
+                let res_sd = await fetch(`https://sdxw.iqilu.com/v2/app/media/program/video/lists?cateid=${cateid}&plat=cq&date=${dateISOStringSD}&page=1`, {
+                    "headers": {
+                        "accept": "*/*",
+                        "accept-language": "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7",
+                        "cache-control": "no-cache",
+                        "content-type": "application/x-www-form-urlencoded",
+                        "cq-token": "",
+                        "orgid": "21",
+                        "pragma": "no-cache",
+                        "sec-fetch-dest": "empty",
+                        "sec-fetch-mode": "cors",
+                        "sec-fetch-site": "same-origin",
+                        "cookie": "Hm_lvt_4306b4ece03aa3d1c0648f71c5cc71fc=1732361356; HMACCOUNT=73122896BC3D293B; Hm_lpvt_4306b4ece03aa3d1c0648f71c5cc71fc=1732361402",
+                        "Referer": "https://sdxw.iqilu.com/share/dHYtMjEtNTYyNjU0OQ.html",
+                        "Referrer-Policy": "strict-origin-when-cross-origin"
+                    },
+                    "body": null,
+                    "method": "GET"
+                });
+
+                let json_sd = await res_sd.json();
+                // console.log(json_sd.data.infos[0].media[0]);
+                if (json_sd.data.infos.length == 0) {
+                    continue;
+                }
+                if (latestIndex == 0) {
+                    latests.push({ text: `#EXTINF:-1 group-title="最新天气",${group_sd_names[index]}${dateISOStringSD.slice(-2)}日${group_sd_times[index]}\n${json_sd.data.infos[0].media[0].url}`, pubDate: `${dateISOStringSD}T${group_sd_times[index]}:00.000+08:00` });
+
+                }
+                m3utext += `\n#EXTINF:-1 group-title="${group_sd_names[index]}",${dateISOStringSD.slice(-5).replace('-', '月')}日${group_sd_times[index]}\n${json_sd.data.infos[0].media[0].url}`;
+                ++latestIndex;
+
+            }
+        }
+
+        // console.log({latests});
+
+        // m3utext = `#EXTM3U\n${latests.sort((a,b)=>new Date(b.pubDate)-new Date(a.pubDate)).map(item=>item.text).join('\n')}${m3utext}`
+        // console.log(m3utext);
+
+    }
+    // return
+
+    if (1) {
+        // 中央气象，数据源1
+
+        let urls = [
+            'https://www.weather.com.cn/pubm/video_lianbo_2021.htm',
+            'https://www.weather.com.cn/pubm/zhaowen.htm',
+            'https://www.weather.com.cn/pubm/wujian.htm',
+            'https://www.weather.com.cn/pubm/diyiyinxiang.htm',
+            'https://www.weather.com.cn/pubm/cctv4.htm',
+            'https://www.weather.com.cn/pubm/tiyu.htm',
+            'https://www.weather.com.cn/pubm/cctv7.htm',
+            // 'https://www.weather.com.cn/pubm/cgtn.htm',
+            // 'https://www.weather.com.cn/pubm/nongye.htm'
+        ];
+
+        let group_names = [
+            '中央一晚',
+            '中央13早',
+            '中央一午',
+            '中央二早',
+            '中央四',
+            '体育天气',
+            '中央七',
+            // '英语台',
+            // '农业气象'
+        ];
+
+        for (let i = 0; i < urls.length; i++) {
+            const url = urls[i];
+            const group_name = group_names[i];
+            let res = await fetch(`${url}?callback=getLbDatas&_=${Date.now()}`, {
+                "cache": "default",
+                "credentials": "include",
                 "headers": {
-                    "accept": "*/*",
-                    "accept-language": "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7",
-                    "cache-control": "no-cache",
-                    "content-type": "application/x-www-form-urlencoded",
-                    "cq-token": "",
-                    "orgid": "21",
-                    "pragma": "no-cache",
-                    "sec-fetch-dest": "empty",
-                    "sec-fetch-mode": "cors",
-                    "sec-fetch-site": "same-origin",
-                    "cookie": "Hm_lvt_4306b4ece03aa3d1c0648f71c5cc71fc=1732361356; HMACCOUNT=73122896BC3D293B; Hm_lpvt_4306b4ece03aa3d1c0648f71c5cc71fc=1732361402",
-                    "Referer": "https://sdxw.iqilu.com/share/dHYtMjEtNTYyNjU0OQ.html",
-                    "Referrer-Policy": "strict-origin-when-cross-origin"
+                    "Accept": "*/*",
+                    "Accept-Language": "zh-CN,zh-Hans;q=0.9",
+                    "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 16_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.4 Mobile/15E148 Safari/604.1"
                 },
-                "body": null,
-                "method": "GET"
+                "method": "GET",
+                "mode": "cors",
+                "redirect": "follow",
+                "referrer": "http://www.weather.com.cn/",
+                "referrerPolicy": "strict-origin-when-cross-origin"
             });
 
-            let json_sd = await res_sd.json();
-            // console.log(json_sd.data.infos[0].media[0]);
-            if (json_sd.data.infos.length == 0) {
-                continue;
+            let text = await res.text();
+            let data = JSON.parse(text.slice(text.indexOf('(') + 1, text.lastIndexOf(')')));
+            // console.log(data);
+            if (group_name != '中央四') {
+
+                let tvg_name = `${group_name}${data.data[0].pubDate.slice(-11, -3).replace(' ', '日')}`;
+                latests.push({ text: `#EXTINF:-1 group-title="最新天气",${tvg_name}\n${data.data[0].url}`, pubDate: `${data.data[0].pubDate.replace(' ', 'T')}.000+08:00` });
+
             }
-            if (latestIndex == 0) {
-                latests.push({text:`#EXTINF:-1 group-title="最新天气",${group_sd_names[index]}${dateISOStringSD.slice(-2)}日${group_sd_times[index]}\n${json_sd.data.infos[0].media[0].url}`,pubDate:`${dateISOStringSD}T${group_sd_times[index]}:00.000+08:00`});
-                
-            }
-            m3utext += `\n#EXTINF:-1 group-title="${group_sd_names[index]}",${dateISOStringSD.slice(-5).replace('-','月')}日${group_sd_times[index]}\n${json_sd.data.infos[0].media[0].url}`;
-            ++latestIndex;
+
+            const playlist = data.data.map((item, index) => {
+                // console.log(item);
+                let _tvg_name = item.pubDate.slice(-14, -3).replace('-', '月').replace(' ', '日');
+                if (item.title == '《天气预报》04:54') {
+                    if (index < 3) {
+                        latests.push({ text: `#EXTINF:-1 group-title="最新天气",北半球气象${_tvg_name.slice(-8, -5)}04:54\n${item.url}`, pubDate: `${item.pubDate.slice(-19, -8).replace(' ', 'T')}04:54:00.000+08:00` });
+                    }
+                    return `#EXTINF:-1 group-title="北半球气象",${item.pubDate.slice(-14, -8).replace('-', '月').replace(' ', '日')}04:54\n${item.url}`
+                }
+                if (item.title == '《天气预报》12:54') {
+                    if (index < 3) {
+                        latests.push({ text: `#EXTINF:-1 group-title="最新天气",中央四午${_tvg_name.slice(-8, -5)}12:54\n${item.url}`, pubDate: `${item.pubDate.slice(-19, -8).replace(' ', 'T')}12:54:00.000+08:00` });
+                    }
+                    return `#EXTINF:-1 group-title="中央四午",${item.pubDate.slice(-14, -8).replace('-', '月').replace(' ', '日')}12:54\n${item.url}`
+                }
+                if (item.title == '《天气预报》21:58') {
+                    if (index < 3) {
+                        latests.push({ text: `#EXTINF:-1 group-title="最新天气",中央四晚${_tvg_name.slice(-8, -5)}21:58\n${item.url}`, pubDate: `${item.pubDate.slice(-19, -8).replace(' ', 'T')}21:58:00.000+08:00` });
+                    }
+                    return `#EXTINF:-1 group-title="中央四晚",${item.pubDate.slice(-14, -8).replace('-', '月').replace(' ', '日')}21:58\n${item.url}`
+                }
+                return `#EXTINF:-1 group-title="${group_name}",${_tvg_name}\n${item.url}`
+            }).join('\n');
+
+            m3utext += `\n${playlist}`;
 
         }
+
     }
 
-    // console.log({latests});
+    if (1) {
+        // 农业气象 数据源2
 
-    // m3utext = `#EXTM3U\n${latests.sort((a,b)=>new Date(b.pubDate)-new Date(a.pubDate)).map(item=>item.text).join('\n')}${m3utext}`
-    // console.log(m3utext);
-
-
-    // return
-    let urls = [
-        'https://www.weather.com.cn/pubm/video_lianbo_2021.htm',
-        'https://www.weather.com.cn/pubm/zhaowen.htm',
-        'https://www.weather.com.cn/pubm/wujian.htm',
-        'https://www.weather.com.cn/pubm/diyiyinxiang.htm',
-        'https://www.weather.com.cn/pubm/cctv4.htm',
-        'https://www.weather.com.cn/pubm/tiyu.htm',
-        'https://www.weather.com.cn/pubm/cctv7.htm',
-        // 'https://www.weather.com.cn/pubm/cgtn.htm',
-        // 'https://www.weather.com.cn/pubm/nongye.htm'
-    ];
-    
-    let group_names = [
-        '中央一晚',
-        '中央13早',
-        '中央一午',
-        '中央二早',
-        '中央四',
-        '体育天气',
-        '中央七',
-        // '英语台',
-        // '农业气象'
-    ];
-        
-    for (let i = 0; i < urls.length; i++) {
-        const url = urls[i];
-        const group_name = group_names[i];
-        let res = await fetch(`${url}?callback=getLbDatas&_=${Date.now()}`, {
-            "cache": "default",
-            "credentials": "include",
+        let reswb = await fetch("https://m.weibo.cn/api/container/getIndex?luicode=10000011&lfid=1005051969156553&type=uid&value=1969156553&containerid=1076031969156553", {
             "headers": {
-                "Accept": "*/*",
-                "Accept-Language": "zh-CN,zh-Hans;q=0.9",
-                "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 16_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.4 Mobile/15E148 Safari/604.1"
+              "accept": "application/json, text/plain, */*",
+              "accept-language": "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7",
+              "cache-control": "no-cache",
+              "mweibo-pwa": "1",
+              "pragma": "no-cache",
+              "priority": "u=1, i",
+              "sec-ch-ua": "\"Google Chrome\";v=\"131\", \"Chromium\";v=\"131\", \"Not_A Brand\";v=\"24\"",
+              "sec-ch-ua-mobile": "?0",
+              "sec-ch-ua-platform": "\"macOS\"",
+              "sec-fetch-dest": "empty",
+              "sec-fetch-mode": "cors",
+              "sec-fetch-site": "same-origin",
+              "x-requested-with": "XMLHttpRequest",
+              "x-xsrf-token": "02e996",
+              "cookie": "_T_WM=26681150684; MLOGIN=0; WEIBOCN_FROM=1110106030; XSRF-TOKEN=02e996; mweibo_short_token=c86832c9b3; M_WEIBOCN_PARAMS=luicode%3D10000011%26lfid%3D1005051969156553%26oid%3D5103586063945698%26fid%3D1005051969156553%26uicode%3D10000011",
+              "Referer": "https://m.weibo.cn/u/1969156553?luicode=10000011&lfid=1005051969156553",
+              "Referrer-Policy": "strict-origin-when-cross-origin"
             },
-            "method": "GET",
-            "mode": "cors",
-            "redirect": "follow",
-            "referrer": "http://www.weather.com.cn/",
-            "referrerPolicy": "strict-origin-when-cross-origin"
+            "body": null,
+            "method": "GET"
         });
-
-        let text = await res.text();
-        let data = JSON.parse(text.slice(text.indexOf('(') + 1, text.lastIndexOf(')')));    
-        // console.log(data);
-        if (group_name != '中央四') {
-
-            let tvg_name = `${group_name}${data.data[0].pubDate.slice(-11,-3).replace(' ','日')}`;
-            latests.push({text:`#EXTINF:-1 group-title="最新天气",${tvg_name}\n${data.data[0].url}`,pubDate:`${data.data[0].pubDate.replace(' ','T')}.000+08:00`});
-                
-        } 
-        
-        const playlist = data.data.map((item,index) => {
-            // console.log(item);
-            let _tvg_name = item.pubDate.slice(-14,-3).replace('-','月').replace(' ','日');
-            if (item.title == '《天气预报》04:54') {
-                if (index < 3) {
-                    latests.push({text:`#EXTINF:-1 group-title="最新天气",北半球气象${_tvg_name.slice(-8,-5)}04:54\n${item.url}`,pubDate:`${item.pubDate.slice(-19,-8).replace(' ','T')}04:54:00.000+08:00`});
-                }
-                return `#EXTINF:-1 group-title="北半球气象",${item.pubDate.slice(-14,-8).replace('-','月').replace(' ','日')}04:54\n${item.url}`                
+    
+        let json = await reswb.json();
+    
+        json.data.cards.forEach((card, index) => {
+            let duration = card.mblog.page_info.media_info.duration
+            let datewbISOString = toISODateFMTbyTimezoneOffset(new Date(card.mblog.created_at), -480);
+            // console.log(`# ${datewbISOString}`);
+            let url = card.mblog.page_info.urls.mp4_720p_mp4
+    
+            if (card.card_type !== 9) return;
+            if (index == 0) {
+                latests.push({ text: `#EXTINF:${duration} group-title="最新天气",农业气象${datewbISOString.slice(8, 16).replace('T', '日')}\n${url}`, pubDate: datewbISOString });
             }
-            if (item.title == '《天气预报》12:54') {
-                if (index < 3) {
-                    latests.push({text:`#EXTINF:-1 group-title="最新天气",中央四午${_tvg_name.slice(-8,-5)}12:54\n${item.url}`,pubDate:`${item.pubDate.slice(-19,-8).replace(' ','T')}12:54:00.000+08:00`});
-                }
-                return `#EXTINF:-1 group-title="中央四午",${item.pubDate.slice(-14,-8).replace('-','月').replace(' ','日')}12:54\n${item.url}`                
-            }
-            if (item.title == '《天气预报》21:58') {
-                if (index < 3) {
-                    latests.push({text:`#EXTINF:-1 group-title="最新天气",中央四晚${_tvg_name.slice(-8,-5)}21:58\n${item.url}`,pubDate:`${item.pubDate.slice(-19,-8).replace(' ','T')}21:58:00.000+08:00`});
-                }
-                return `#EXTINF:-1 group-title="中央四晚",${item.pubDate.slice(-14,-8).replace('-','月').replace(' ','日')}21:58\n${item.url}`                
-            }
-            return `#EXTINF:-1 group-title="${group_name}",${_tvg_name}\n${item.url}`
-        }).join('\n');
-
-        m3utext += `\n${playlist}`;     
-
+            m3utext += `\n#EXTINF:${duration} group-title="农业气象",${datewbISOString.slice(5, 16).replace('-', '月').replace('T', '日')}\n${url}`
+    
+        });
+    
     }
-
-    let reswb = await fetch("https://m.weibo.cn/api/container/getIndex?luicode=10000011&lfid=1005051969156553&type=uid&value=1969156553&containerid=1076031969156553", {
-        "headers": {
-          "accept": "application/json, text/plain, */*",
-          "accept-language": "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7",
-          "cache-control": "no-cache",
-          "mweibo-pwa": "1",
-          "pragma": "no-cache",
-          "priority": "u=1, i",
-          "sec-ch-ua": "\"Google Chrome\";v=\"131\", \"Chromium\";v=\"131\", \"Not_A Brand\";v=\"24\"",
-          "sec-ch-ua-mobile": "?0",
-          "sec-ch-ua-platform": "\"macOS\"",
-          "sec-fetch-dest": "empty",
-          "sec-fetch-mode": "cors",
-          "sec-fetch-site": "same-origin",
-          "x-requested-with": "XMLHttpRequest",
-          "x-xsrf-token": "02e996",
-          "cookie": "_T_WM=26681150684; MLOGIN=0; WEIBOCN_FROM=1110106030; XSRF-TOKEN=02e996; mweibo_short_token=c86832c9b3; M_WEIBOCN_PARAMS=luicode%3D10000011%26lfid%3D1005051969156553%26oid%3D5103586063945698%26fid%3D1005051969156553%26uicode%3D10000011",
-          "Referer": "https://m.weibo.cn/u/1969156553?luicode=10000011&lfid=1005051969156553",
-          "Referrer-Policy": "strict-origin-when-cross-origin"
-        },
-        "body": null,
-        "method": "GET"
-    });
-
-    let json = await reswb.json();
-
-    json.data.cards.forEach((card, index) => {
-        let duration = card.mblog.page_info.media_info.duration
-        let datewbISOString = toISODateFMTbyTimezoneOffset(new Date(card.mblog.created_at), -480);
-        // console.log(`# ${datewbISOString}`);
-        let url = card.mblog.page_info.urls.mp4_720p_mp4
-
-        if (card.card_type !== 9) return;
-        if (index == 0) {
-            latests.push({ text: `#EXTINF:${duration} group-title="最新天气",农业气象${datewbISOString.slice(8, 16).replace('T', '日')}\n${url}`, pubDate: datewbISOString });
-        }
-        m3utext += `\n#EXTINF:${duration} group-title="农业气象",${datewbISOString.slice(5, 16).replace('-', '月').replace('T', '日')}\n${url}`
-
-    });
 
     // console.log(latests);
     m3utext = `#EXTM3U\n${latests.sort((a,b)=>new Date(b.pubDate)-new Date(a.pubDate)).map(item=>item.text).join('\n')}${m3utext}`
